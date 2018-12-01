4507d37b4e53e804f649d5d3e663bd85eda5963a
SkyLu <2924312854@qq.com>
2018-12-01 20:07:04 +0800

    add gcc practice

---
+++ struct/struct_array/keywords.c
@@ -0,0 +1,9 @@
+#include "keywords.h"
+
+extern int getword(char *word, int lim);
+extern int binsearch(char *word, struct key tab[], int n);
+
+int main(int argc, char const *argv[]) {
+
+    return 0;
+}
