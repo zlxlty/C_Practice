a9136543c59155108ad24774a30acee769d39035
SkyLu <2924312854@qq.com>
2018-12-01 12:30:19 +0800

    add some magical c programs

---
+++ magic/moon.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <math.h>
+double l;
+main(_,o,O){return putchar((_--+22&&_+50&&main(_,-49,_),_&&o)?  (main(-49,o+1,O),((l=(o+25)/sqrt(3-O*22-O*O), l*l<4&&(fabs(((time(0)-607728)%2551443)/405859.-4.7+acos(l/2))<1.57))[" #"])):10);}
