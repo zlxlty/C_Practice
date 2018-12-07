cc02489dbc6bd5adb88e7a7e009e4f0bdb889c09
SkyLu <2924312854@qq.com>
2018-12-07 15:40:33 +0800

    add structure union

--- union/union_test.c
+++ union/union_test.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef char * string;
+#define NSYM 100
 
-typedef union ntype_data
-{
-    int i_data;
-    float f_data;
-    double d_data;
-    string s_data;
-} Data;
+struct {
+    char *name;
+    int flags;
+    int utype;
+    union {
+        int val;
+        float fval;
+        char *val;
+    } u;
+} symtab[NSYM];
 
 int main(int argc, char const *argv[]) {
-    Data d;
-    d.i_data = 1;
-    string str = "helllo";
-    d.s_data = str;
-    // d.f_data = 1.2;
-    printf("%s\n", d.s_data);
+    symtab[0].val = 0;
     return 0;
 }
