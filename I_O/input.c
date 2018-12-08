1f5b37db4c6cf8eaa43e535290165d86a5fd9dcb
SkyLu <2924312854@qq.com>
2018-12-08 20:36:56 +0800

    add i/o practice

---
+++ I_O/input.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    putchar('h');
+    return 0;
+}
---
+++ I_O/output.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    char ch;
+    ch = getchar();
+    printf("received: %c\n", ch);
+    return 0;
+}
